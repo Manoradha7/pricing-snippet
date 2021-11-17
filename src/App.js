ad2693ae48a67b537a8c24702f9e04275785e3f4
Manoradha7 <manoradha7@gmail.com>
2021-11-17 22:56:55 +0530

    pricing snippet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "@mui/material/Button";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card />
+      <Card1 />
+      <Card2 />
     </div>
   );
 }
 
-export default App;
+// free card detils
+function Card() {
+  return (
+    <div className="card-content">
+      <div class="card-head">
+        <h5 className="text-hide">
+          <center>FREE</center>
+        </h5>
+        <h6>
+          <center>
+            $0<span className="period">/month</span>
+          </center>
+        </h6>
+      </div>
+      <hr />
+
+      {/*card body part*/}
+      <div className="card-body">
+        <ul>
+          <li> ✔ Single User </li>
+          <li> ✔ 5GB Storage </li>
+          <li> ✔ Unlimited Public Projects </li>
+          <li> ✔ Community Access </li>
+          <li className="text-hide"> ❌ Unlimited Private Projects</li>
+          <li className="text-hide"> ❌ Dedicated Phone Support</li>
+          <li className="text-hide">❌ Free Subdomain</li>
+          <li className="text-hide"> ❌ Monthly Status Reports</li>
+        </ul>
+      </div>
+      <div className="buy">
+        <Button className="btn" variant="contained">
+          BUTTON
+        </Button>
+      </div>
+    </div>
+  );
+}
+// Plus Card details
+function Card1() {
+  return (
+    <div className="card-content">
+      {/*card head part*/}
+      <div class="card-head">
+        <h5 className="text-hide">
+          <center>PLUS</center>
+        </h5>
+        <h6>
+          <center>
+            $9<span className="period">/month</span>
+          </center>
+        </h6>
+      </div>
+      <hr />
+
+      {/*card body part*/}
+      <div className="card-body">
+        <ul>
+          <li>
+            ✔ <b>5 Users</b>
+          </li>
+          <li> ✔ 50GB Storage </li>
+          <li> ✔ Unlimited Public Projects </li>
+          <li> ✔ Community Access </li>
+          <li> ✔ Unlimited Private Projects </li>
+          <li> ✔ Dedicated Phone Support </li>
+          <li> ✔ Free Subdomain </li>
+          <li className="text-hide">❌ Monthly Status Reports</li>
+        </ul>
+      </div>
+      <div className="buy">
+        <Button className="btn" variant="contained">
+          BUTTON
+        </Button>
+      </div>
+    </div>
+  );
+}
+// Pro Card details
+function Card2() {
+  return (
+    <div className="card-content">
+      {/*card head part*/}
+      <div class="card-head">
+        <h5 className="text-hide">
+          <center>PRO</center>
+        </h5>
+        <h6>
+          <center>
+            $49<span className="period">/month</span>
+          </center>
+        </h6>
+      </div>
+      <hr />
+
+      {/*card body part*/}
+      <div className="card-body">
+        <ul>
+          <li>✔ <b>Unlimited Users</b></li>
+          <li> ✔ 150GB Storage </li>
+          <li> ✔ Unlimited Public Projects </li>
+          <li> ✔ Community Access </li>
+          <li> ✔ Unlimited Private Projects </li>
+          <li> ✔ Dedicated Phone Support </li>
+          <li>✔ <b>Unlimited</b> Free Subdomain</li>
+          <li> ✔ Monthly Status Reports </li>
+        </ul>
+      </div>
+      <div className="buy">
+        <Button className="btn" variant="contained">
+          BUTTON
+        </Button>
+      </div>
+    </div>
+  );
+}
